69d3158cf76930f2001ba6d3ff856f82c481b7f4
aoshido <aoshido@gmail.com>
2024-01-07 23:46:47 -0300

    Added logging
      - Add monolog bundle
      - Add logging instead of a dump in the consumer
      - Add consumer to supervisord

--- src/MessageHandler/PossibleDuplicateHandler.php
+++ src/MessageHandler/PossibleDuplicateHandler.php
@@ -4,10 +4,15 @@ namespace App\MessageHandler;
 
 use App\Message\PossibleDuplicate;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Psr\Log\LoggerInterface;
 
 #[AsMessageHandler]
 class PossibleDuplicateHandler {
+
+    public function __construct(private LoggerInterface $logger) {
+    }
+
     public function __invoke(PossibleDuplicate $message) {
-        // ... do some work - like sending an SMS message!
+        $this->logger->info($message->getContent());
     }
 }
